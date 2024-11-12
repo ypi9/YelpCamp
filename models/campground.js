4725c785356a32ddd36382a7b3422b3093c61ac3
ypi9 <ypi@seas.upenn.edu>
2024-11-12 15:30:35 -0800

    add campgrounds CRUD

---
+++ models/campground.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CampgroundSchema = new Schema({
+    title: String,
+    price: String,
+    description: String,
+    location: String
+});
+
+//export
+module.exports = mongoose.model('Campground', CampgroundSchema);
