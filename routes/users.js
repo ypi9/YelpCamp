6fab90db9bd8af09447cee308c7f6fedce7745e0
ypi9 <ypi@seas.upenn.edu>
2024-12-16 11:52:34 -0800

    refactor routes

--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,14 @@ const User = require('../models/user');
 const { storeReturnTo } = require('../middleware');
 const users = require('../controllers/users');
 
-router.get('/register', users.renderRegister);
-router.post('/register', catchAsync(users.register));
-router.get('/login', users.renderLogin)
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
+
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
+
 router.get('/logout', users.logout)
 
 module.exports = router;
