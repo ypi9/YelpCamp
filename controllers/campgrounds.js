060404ef9e6cfcce60032cd69e1cd0bf45219da7
ypi9 <ypi@seas.upenn.edu>
2024-12-18 15:08:42 -0800

    add image upload

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -11,8 +11,10 @@ module.exports.renderNewForm = (req, res) => {
 
 module.exports.createCampground = async (req, res, next) => {
     const campground = new Campground(req.body.campground);
+    campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.author = req.user._id;
     await campground.save();
+    console.log(campground);
     req.flash('success', 'Successfully made a new campground!');
     res.redirect(`/campgrounds/${campground._id}`)
 }
@@ -22,9 +24,8 @@ module.exports.showCampground = async (req, res,) => {
         path: 'reviews',
         populate: {
             path: 'author',
-            select: 'username'
         }
-    }).populate('author', 'username');
+    }).populate('author');
     if (!campground) {
         req.flash('error', 'Cannot find that campground!');
         return res.redirect('/campgrounds');
