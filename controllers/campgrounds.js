5211498a318d8bd29c672f696ba0c4595173eed6
ypi9 <ypi@seas.upenn.edu>
2024-12-19 22:14:43 -0800

    Revert "add basic security stuff"
    This reverts commit 1ba5fba48c8489a1df3c8934abfe23ecdb168a08.

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,8 +1,6 @@
 const Campground = require('../models/campground');
 const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
-
 const mapBoxToken = process.env.MAPBOX_TOKEN;
-console.log('Using Mapbox Token:', mapBoxToken);
 const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 const { cloudinary } = require("../cloudinary");
 
